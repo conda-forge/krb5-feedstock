03f3266d0dc30e658e0d3acd6d45b7f51e651634
Ray Donnelly <mingw.android@gmail.com>
2017-09-15 06:55:51 -0700

    Fix macOS build and add features

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+if [[ ${HOST} =~ .*darwin.* ]]; then
+  export LDFLAGS="${LDFLAGS_CC} -Wl,-rpath,${PREFIX}/lib"
+fi
+
+export CPPFLAGS="${CPPFLAGS} -I${PREFIX}/include"
+export LDFLAGS="${LDFLAGS} -L${PREFIX}/lib"
+
 pushd src
   autoreconf -i
-  ./configure --prefix=${PREFIX}  \
-              --host=${HOST}
+  ./configure --prefix=${PREFIX}    \
+              --host=${HOST}        \
+              --with-tcl=${PREFIX}  \
+              --without-readline    \
+              --with-libedit
   make -j${CPU_COUNT} ${VERBOSE_AT}
   if [ "${PY_VER}" == "2.7" ]; then
     make check
